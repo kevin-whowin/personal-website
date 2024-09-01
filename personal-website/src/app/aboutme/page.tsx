0b5287ee60533aa98bb8c576819fd300750a5e4c
KevinnH7 <125126476+KevinnH7@users.noreply.github.com>
2024-09-01 23:10:44 +1000

    Added fade in transition to home page

--- personal-website/src/app/aboutme/page.tsx
+++ personal-website/src/app/aboutme/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
 
         <div>
-          <h1 className=" pb-4 text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 sm:text-7xl sm:dark:from-sky-900 sm:dark:to-blue-900">
+          <h1 className=" pb-4 text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 sm:text-7xl">
             Kevin Huynh!
           </h1>
           <div className="flex flex-col space-y-8 mx-20 ">
--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,14 +1,23 @@
+'use client';
+
+import { useFadeIn } from './useFadeIn';
+
 export default function Home() {
+  const fadeRef1 = useFadeIn();
+  const fadeRef2 = useFadeIn();
+
   return (
-    <main className=" bg-black flex min-h-screen flex-col items-center justify-between bg-cover p-24">
-        <div>
-          <h1 className=" pb-4 text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 sm:text-7xl sm:dark:from-sky-900 sm:dark:to-blue-900">
-            Kevin Huynh!
-          </h1>
-          <div className="flex text-white flex-col space-y-8 mx-20 ">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec tincidunt metus. Sed molestie arcu a ligula auctor, eget condimentum dui egestas. Fusce scelerisque ligula ligula, a malesuada nibh maximus vitae. Maecenas tincidunt mi at laoreet commodo. Donec eleifend eros eget eros dapibus, nec vehicula tellus viverra. Donec faucibus magna ac efficitur pellentesque. Proin ultrices est ut tempus eleifend. Vivamus eu porta tortor. Etiam aliquam, nunc eget condimentum dapibus, libero tortor porta nibh, non ullamcorper lacus magna vel quam. Etiam nec malesuada erat. Maecenas accumsan ac odio at mattis. Aenean porta posuere scelerisque. 
-          </div>
+    <main className="bg-black flex min-h-screen flex-col items-center justify-between bg-cover p-24">
+      <div ref={fadeRef1} className="opacity-0 transition-opacity duration-1000 ease-in-out">
+        <h1 className="pb-4 text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 sm:text-7xl">
+          Kevin Huynh!
+        </h1>
+      </div>
+      <div ref={fadeRef2} className="opacity-0 transition-opacity duration-1000 ease-in-out">
+        <div className="flex text-white flex-col space-y-8 mx-20">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec tincidunt metus. Sed molestie arcu a ligula auctor, eget condimentum dui egestas. Fusce scelerisque ligula ligula, a malesuada nibh maximus vitae. Maecenas tincidunt mi at laoreet commodo. Donec eleifend eros eget eros dapibus, nec vehicula tellus viverra. Donec faucibus magna ac efficitur pellentesque. Proin ultrices est ut tempus eleifend. Vivamus eu porta tortor. Etiam aliquam, nunc eget condimentum dapibus, libero tortor porta nibh, non ullamcorper lacus magna vel quam. Etiam nec malesuada erat. Maecenas accumsan ac odio at mattis. Aenean porta posuere scelerisque.
         </div>
+      </div>
     </main>
   );
 }
