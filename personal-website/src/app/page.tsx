fdf3b365e58ddba5b0e8764f9d04c29ebcca3520
KevinnH7 <125126476+KevinnH7@users.noreply.github.com>
2024-09-01 23:16:43 +1000

    added resume button

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -12,9 +12,10 @@ export default function Home() {
         <h1 className="pb-4 text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 sm:text-7xl">
           Kevin Huynh!
         </h1>
+        <a href="/cv.pdf" download="cv">Download CV</a>
       </div>
       <div ref={fadeRef2} className="opacity-0 transition-opacity duration-1000 ease-in-out">
-        <div className="flex text-white flex-col space-y-8 mx-20">
+        <div className="flex text-white flex-col space-y-8 mx-40 pb-60">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec tincidunt metus. Sed molestie arcu a ligula auctor, eget condimentum dui egestas. Fusce scelerisque ligula ligula, a malesuada nibh maximus vitae. Maecenas tincidunt mi at laoreet commodo. Donec eleifend eros eget eros dapibus, nec vehicula tellus viverra. Donec faucibus magna ac efficitur pellentesque. Proin ultrices est ut tempus eleifend. Vivamus eu porta tortor. Etiam aliquam, nunc eget condimentum dapibus, libero tortor porta nibh, non ullamcorper lacus magna vel quam. Etiam nec malesuada erat. Maecenas accumsan ac odio at mattis. Aenean porta posuere scelerisque.
         </div>
       </div>
